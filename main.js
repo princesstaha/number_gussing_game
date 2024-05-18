904b1c90d88d8957a9016c3d8d0c8b9afbc72722
princesstaha <159701680+princesstaha@users.noreply.github.com>
2024-05-18 22:30:11 +0500

    Add files via upload
    done

---
+++ main.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type: "number",
+        message: "please guess a number between 1-10:",
+    },
+]);
+if (answers.userGussedNumber === randomNumber) {
+    console.log("Congratulations! you guessed the right number,");
+}
+else {
+    console.log("You gussed wrong number");
+}
